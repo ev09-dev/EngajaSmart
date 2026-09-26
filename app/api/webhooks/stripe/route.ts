c20abb88e069625bf30a67d88210778263ba7c1d
evaristo <eyambam@gmail.com>
2026-09-26 20:21:46 +0100

    Corrige apiVersion para 2023-10-16

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 import { supabase } from '@/lib/supabase'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: Stripe.LatestApiVersion,
+  apiVersion: '2023-10-16', 
 })
 
 const priceIdToPlan: { [key: string]: string } = {
